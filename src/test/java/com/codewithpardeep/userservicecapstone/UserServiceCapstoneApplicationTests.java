3018571fc75726c5ea9a58fe4a38130f68c7b3d5
Pardeep Chahal <pardeepchahal89@gmail.com>
2025-05-29 09:11:51 +0530

    Initial commit - create project

---
+++ src/test/java/com/codewithpardeep/userservicecapstone/UserServiceCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithpardeep.userservicecapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
